444c090da63f35128bc39e5b3e8ca78250b77494
rodriquelali <rodriquelali@gmail.com>
2026-03-12 18:50:25 -0400

    feat(code): Configutacion UI.

--- MyCarsProyect/app/src/main/java/com/example/mycarsproyect/MainActivity.kt
+++ MyCarsProyect/app/src/main/java/com/example/mycarsproyect/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.mycarsproyect
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mycarsproyect.databinding.ActivityLoginBinding
+import com.example.mycarsproyect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var butIni = binding.loginInicio
+        var ceunta = binding.crearCuenta
+
+        butIni.setOnClickListener {
+            Toast.makeText(this, "Hola Login", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
